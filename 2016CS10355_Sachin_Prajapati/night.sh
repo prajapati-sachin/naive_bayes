c9e1d916a9cdb65398c31db036d4f59b6c4bb2a5
prajapati-sachin <sachinkp1211@gmail.com>
2019-03-15 18:57:48 +0530

    Almost Done

--- 2016CS10355_Sachin_Prajapati/night.sh
+++
@@ -1,9 +0,0 @@
-export PYTHONPATH="/home/sachin/libsvm-3.23/python:${PYTHONPATH}"
-#!/bin/bash
-
-echo "Running first"
-./run.sh 1 ../p1/train.json ../p1/test.json d > d.txt
-echo "Running second"
-./run.sh 1 ../p1/train.json ../p1/test.json f > f.txt
-echo "Running third"
-./run.sh 1 ../p1/train_full.json ../p1/test.json g > g.txt
